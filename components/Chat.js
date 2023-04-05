d601c7e223927169ceee94698dd1e4f18c88cd95
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-04-04 23:23:46 -0400

    added GiftedChat library, implemented basic chat UI

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,77 @@
-import { StyleSheet, View, Text, PlatformColor } from 'react-native';
-import { useEffect } from 'react';
+import { StyleSheet, View, Button, Platform, KeyboardAvoidingView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 // using route and navigation to gather name and color values passed from Start.js
-const Chat = ({ route, navigation }) => {
+// creates State for messages and setMessages
+export default function Chat({ route, navigation }) {
     const { name, color } = route.params;
+    const [messages, setMessages] = useState([]);
 
     //sets the title bar and background color to name and color passed by state from Start.js
     useEffect(() => {
         navigation.setOptions({ title: name, backgroundColor: color });
     }, []);
-    
+
+    //testing messages by adding the first message as well as a system message
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ]);
+    }, []);
+
+    //appends new messages to previous messages
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    //changes the properties of chat bubbles
+    const renderBubble = (props) => {
+        return <Bubble
+            {...props}
+            wrapperStyle={{
+                right: {
+                    backroundColor: "#000"
+                },
+                left: {
+                    backgroundColor: "#FFF"
+                }
+            }}
+        />
+    }
+
     return (
-        <View style={[styles.container, { backgroundColor: color}]}>
-            <Text>Welcome to Chat</Text>
+        <View style={styles.container}>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                    name,
+                    avatar: "https://placebear.com/140/140",
+                }}
+            />
+
+            {/* keeps android and ios keyboards functioning properly */}
+            { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+            { Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" /> : null }
+            <Button title="Leave Chat" onPress={() => navigation.navigate("Start")} />
         </View>
     );
 }
@@ -20,9 +79,5 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
  container: {
    flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
  }
 });
-
-export default Chat;
